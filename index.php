50e7ef4b7ddb9c174b095deef2a28dd2283d5d1d
heitordeve <heitor.developer@outlook.com>
2024-10-03 14:45:29 -0300

    FEAT - Listar usuários no banco de dados

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM user");
+if($sql->rowCount() > 0){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <h1>Listagem de Usuários</h1>
@@ -11,6 +17,18 @@ require 'config.php';
         <th>E-mail</th>
         <th>Ações</th>
     </tr>
+
+    <?php foreach($lista as $user): ?>
+        <tr>
+            <td><?=$user['id'];?></td>
+            <td><?=$user['name'];?></td>
+            <td><?=$user['email'];?></td>
+            <td>
+                <a href="editar.php?id=<?=$user['id'];?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$user['id'];?>">[ Excluir ]</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
 </table>
 
 <a href="cadastrar.php">Cadastrar Usuários</a>
